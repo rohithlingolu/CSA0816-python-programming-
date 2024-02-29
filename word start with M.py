82fb4c3adff8a01656032205ed1e201dc2ab351a
rohithlingolu <126038924+rohithlingolu@users.noreply.github.com>
2024-02-29 09:58:30 +0530

    Add files via upload

---
+++ word start with M.py
@@ -0,0 +1,7 @@
+string=input("Enter a sentence: ")
+  wordcount=0
+  conv= stringsplit()
+  for word in string:
+      if(wor[0]== 'M'):
+          wordcount+=1
+print("words with M  is " wordcount)
